4cf105f23a4dd74253f8717fc1ac7c7309ccf83b
DarkAlekxxSP19 <niko.beelik@hotmail.com>
2023-07-13 20:06:26 -0600

    v1

--- web/JS/Dashboard.js
+++ web/JS/Dashboard.js
@@ -1,37 +1,31 @@
-
-
-function cargarModuloAlumnos(){
-var contenedor = document.getElementById('content');
-
-// Realizar petición Fetch
-fetch('../Modules/ModuloAlumnos/Alumnos.html')
-  .then(function(response) {
-    return response.text();
-  })
-  .then(function(html) {
-    // Insertar el contenido HTML en el contenedor
-    contenedor.innerHTML = html;
-  })
-  .catch(function(error) {
-    console.log('Error:', error);
-  });
-
-}
-
-function cargarModuloMaestros(){
-  var contenedor = document.getElementById('content');
-  
+function cargarModuloAlumnos() {
+  var contenedor = document.getElementById("content");
   // Realizar petición Fetch
-  fetch('../Modules/ModuloMaestros/Maestros.html')
-    .then(function(response) {
+  fetch("../Modules/ModuloAlumnos/Alumnos.html")
+    .then(function (response) {
       return response.text();
     })
-    .then(function(html) {
+    .then(function (html) {
       // Insertar el contenido HTML en el contenedor
       contenedor.innerHTML = html;
     })
-    .catch(function(error) {
-      console.log('Error:', error);
+    .catch(function (error) {
+      console.log("Error:", error);
     });
-  
-  }
+}
+
+function cargarModuloMaestros() {
+  var contenedor = document.getElementById("content");
+  // Realizar petición Fetch
+  fetch("../Modules/ModuloMaestros/Maestros.html")
+    .then(function (response) {
+      return response.text();
+    })
+    .then(function (html) {
+      // Insertar el contenido HTML en el contenedor
+      contenedor.innerHTML = html;
+    })
+    .catch(function (error) {
+      console.log("Error:", error);
+    });
+}
