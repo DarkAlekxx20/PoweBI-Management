3d56aba47fab9c138135bed779898b92f5c4e579
DarkAlekxxSP19 <niko.beelik@hotmail.com>
2023-07-13 21:38:47 -0600

    v1

--- web/JS/Dashboard.js
+++ web/JS/Dashboard.js
@@ -1,7 +1,7 @@
 let moduloAlumnos;
 let moduloMaestros;
 
-function cargarModuloAlumnos() {
+function cargarModuloAlumnos(){
   fetch("../Modules/ModuloAlumnos/Alumnos.html").then(function(response){
     return response.text();
   }).then(function (html){
@@ -12,13 +12,13 @@ function cargarModuloAlumnos() {
   });
 }
 
-function cargarModuloMaestros() {
+function cargarModuloMaestros(){
   fetch("../Modules/ModuloMaestros/Maestros.html").then(function(response){
     return response.text();
   }).then(function (html){
     document.getElementById("content").innerHTML = html;
     import("../Modules/ModuloMaestros/MaestrosController.js").then(function (controller){
-      moduloAlumnos = controller;
+      moduloMaestros = controller;
     });
   });
 }
